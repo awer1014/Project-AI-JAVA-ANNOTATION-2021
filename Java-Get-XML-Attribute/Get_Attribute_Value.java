f11b8840c1a811f69f7a1a3a12c557aad452bd78
W.R Chen <danny09174354@gmail.com>
2021-03-31 08:37:56 +0800

    update

--- Java-Get-XML-Attribute/Get_Attribute_Value.java
+++ Java-Get-XML-Attribute/Get_Attribute_Value.java
@@ -27,23 +27,25 @@ public class Get_Attribute_Value {
         wtt.create_New_file(file_name);
         for (int i=0;i<Error_List.getLength();i++){
             Element Error = (Element)Error_List.item(i);
-            Element Line = (Element)Line_List.item(i);
             //print Error type
             String type = Error.getAttribute("tpye");
             wtt.write_Error_type(type);
-            //print Error Source code
-            String src = Line.getAttribute("src");
-            wtt.write_Error_src(src);
-            //print Error Begin
-            String Begin = Line.getAttribute("Begin");
-            wtt.write_Error_Begin(Begin);
-            //print Error End
-            String End = Line.getAttribute("End");
-            wtt.write_Error_End(End);
             //System.out.println("Error type: "+type);
             //System.out.println("Source code: "+Src);
             //System.out.println("Begin: "+Begin);
             //System.out.println("End: "+End);
+            for(int k=0;k<Line_List.getLength();k++){
+                Element Line = (Element)Line_List.item(k);
+                //print Error Source code
+                String src = Line.getAttribute("src");
+                wtt.write_Error_src(src);
+                //print Error Begin
+                String Begin = Line.getAttribute("Begin");
+                wtt.write_Error_Begin(Begin);
+                //print Error End
+                String End = Line.getAttribute("End");
+                wtt.write_Error_End(End);
+            }
         }
         //Save file
         wtt.save_file();
