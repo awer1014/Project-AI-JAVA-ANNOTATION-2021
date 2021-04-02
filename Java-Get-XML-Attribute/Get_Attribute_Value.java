2ff7484e96441cdcf31bbfc619f91c7fa60b25fe
W.R Chen <danny09174354@gmail.com>
2021-04-02 15:47:18 +0800

    update
    add Source_Code_Sorter
    and need to add library...

--- Java-Get-XML-Attribute/Get_Attribute_Value.java
+++ Java-Get-XML-Attribute/Get_Attribute_Value.java
@@ -14,15 +14,19 @@ import org.w3c.dom.*;
 import javax.xml.xpath.*;
 import org.xml.sax.SAXException;
 import java.util.*;
+
 public class Get_Attribute_Value {
     public static void get_Value(String file_name, Document doc) throws SAXException, IOException, ParserConfigurationException, XPathExpressionException {
-        
+        //ArrayList-->Mapper
         ArrayList<Line_Block> line_List = new ArrayList<Line_Block>();
         Mapper map = new Mapper();
+
+
         XPathFactory xpf = XPathFactory.newInstance();
         XPath xpath = xpf.newXPath();
+
         NodeList Error_List = (NodeList) xpath.evaluate("/ErrorList/Errors/Error", doc, XPathConstants.NODESET);
-        for (int i=0;i<Error_List.getLength();i++){
+        for (int i = 0; i < Error_List.getLength(); i++){
             Element Error = (Element)Error_List.item(i);
             NodeList Line_List = (NodeList) xpath.evaluate("/ErrorList/Errors/Error/Linelist/line", Error, XPathConstants.NODESET);
             String type = Error.getAttribute("tpye");
@@ -33,7 +37,7 @@ public class Get_Attribute_Value {
             //System.out.println("End: "+End);
             int key = map.get_Index(type);
             Line_Block lb = new Line_Block(key);
-            for(int k=0;k<Line_List.getLength();k++){
+            for(int k = 0; k < Line_List.getLength(); k++){
                 Element Line = (Element)Line_List.item(k);
                 //print Error Source code
                 String src = Line.getAttribute("src");
@@ -49,8 +53,26 @@ public class Get_Attribute_Value {
             line_List.add(lb);
         }
         Collections.sort(line_List);
+
+
         //adjust lines position ...
+        NodeList Source_Code_List = (NodeList) xpath.evaluate("/ErrorList/SourceCode_List/SourceCode", doc, XPathConstants.NODESET);
+        //set ArrayList for SourceCode Attribute
+        ArrayList<String> Source_Code = new ArrayList<>();
+        ArrayList<Integer> Source_code_being_line = new ArrayList<>();
+        //get Source Code Value and add to ArrayList
+        int line_sum = 0;
+        Source_Code_Sorter scs = new Source_Code_Sorter();
+        for (int i = 0; i < Source_Code_List.getLength(); i++){
+            //check ArrayList is empty or not
+            boolean ArrayListisEmpty = Source_Code.isEmpty();
+            //get Source Code
+            Element Source = (Element)Source_Code_List.item(i);
+            String Code_name = Source.getAttribute("name");
+            int code_line = Integer.parseInt(Source.getAttribute("lines"));
+            scs.add_SourceCode_and_line(Code_name, code_line);
+        }
+
     }
 
 }
-
---
+++ Java-Get-XML-Attribute/Source_Code_Sorter.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Source_Code_Sorter {
+    //set ArrayList for SourceCode Attribute
+    ArrayList<String> Source_Code = new ArrayList<>();
+    ArrayList<Integer> Source_code_being_line = new ArrayList<>();
+    //get Source Code Value and add to ArrayList
+    int line_sum = 0;
+    public void add_SourceCode_and_line(String Code_name, int code_line) {
+        boolean ArrayListisEmpty = Source_Code.isEmpty();
+        if (ArrayListisEmpty == true) {
+              Source_Code.add(Code_name);
+              Source_code_being_line.add(0);
+            }
+            else {
+              Source_Code.add(Code_name);
+              Source_code_being_line.add(line_sum);
+            }
+            line_sum += code_line;
+    }
+
+    public String get_SourceCode (int key) {
+      return Source_Code.get(key);
+    }
+
+    public Integer get_Sourceline (int key) {
+      return Source_code_being_line.get(key);
+    }
+
+}
