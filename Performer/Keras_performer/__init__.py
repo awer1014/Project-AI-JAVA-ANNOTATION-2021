3dc0a622bfd8b13290ca251eab95f7b9c8e996ab
W.R Chen <danny09174354@gmail.com>
2021-05-26 22:28:45 +0800

    big update
    can use tensorflow and keras in right way
    now start to training
    issue:
    got some error with training model

--- Performer/Keras_performer/__init__.py
+++
@@ -1,4 +0,0 @@
-from keras_performer import gelu
-from keras_transformer import *
-
-__version__ = '0.38.0'
---
+++ Performer/Perfomer_local/keras_performer/__init__.py
@@ -0,0 +1,4 @@
+from keras_performer.gelu import gelu
+#from transformer import *
+
+__version__ = '0.38.0'
