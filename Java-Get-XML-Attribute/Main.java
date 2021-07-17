8c7ed66781353d466ab8406998f94998ca23179b
OrionZarathos <joccr77586@gmail.com>
2021-07-17 21:36:36 +0800

    V1.0 update
    new version get_XML_attri update
    delete some temp folders

--- Java-Get-XML-Attribute/Main.java
+++ Java-Get-XML-Attribute/Main.java
@@ -39,7 +39,7 @@ public class Main {
             mn.files = mn.xfs.getXmlFileList(mn.Filepath);
             //lx.getNode(files,Filepath);
             rxf.load_Xml_file(mn.files, mn.Filepath, mn.list);
-            mn.ew.write("test.csv", mn.list, rxf.file_id_list);
+            mn.ew.write("test.xls", mn.list, rxf.file_id_list);
         } catch (Exception e) {
             e.printStackTrace();
         }
