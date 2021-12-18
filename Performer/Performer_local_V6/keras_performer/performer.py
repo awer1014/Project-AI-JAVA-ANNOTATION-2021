07e0702fe2eff62501f310a85e81953caa482ed6
W.R Chen <39992080+awer1014@users.noreply.github.com>
2021-12-19 02:21:02 +0800

    fix dropout bug

--- Performer/Performer_local_V6/keras_performer/performer.py
+++ Performer/Performer_local_V6/keras_performer/performer.py
@@ -483,21 +483,21 @@ def get_model(max_input_len,
     #print("flatten_state:", flatten_state.shape)
     error_feed_forward_layer1 = keras.layers.Dense(hidden_dim,
                                                    activation="relu")(flatten_state)
-    error_feed_forward_output1 = keras.layers.Dense(errNum,
+    error_feed_forward_layer1 = keras.layers.Dense(errNum,
                                                     activation="sigmoid",
                                                     name="error_feed_forward_output1")(error_feed_forward_layer1)
     #add dropout layer
-    error_feed_forward_layer1 = keras.layers.Dropout(0.2)(error_feed_forward_output1)
+    error_feed_forward_output1 = keras.layers.Dropout(0.2)(error_feed_forward_layer1)
     #print("flatten_state:", flatten_state.shape)
     #print("error_feed_forward_output1:", error_feed_forward_output1.shape)
-    concatted = keras.layers.Concatenate()([error_feed_forward_output1, flatten_state])
+    #concatted = keras.layers.Concatenate()([error_feed_forward_output1, flatten_state])
 
     #分類器2
 
     LNoutputs=[]
 
     error_feed_forward_layer2 = keras.layers.Dense(hidden_dim,
-                                                   activation="relu")(concatted)
+                                                   activation="relu")(flatten_state)
     error_feed_forward_output2 = keras.layers.Dense(lbNum,
                                                     activation="relu",
                                                     name="error_feed_forward_output2")(error_feed_forward_layer2)
