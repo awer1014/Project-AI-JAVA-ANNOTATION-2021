2945ddf67fa0cdc69f024fec8aaf56438f1a1147
W.R Chen <39992080+awer1014@users.noreply.github.com>
2022-07-21 04:40:59 +0800

    test beautify

--- Code-Augmentation/NewVerPythonDebugger(ref)/testAugmentedCode.py
+++ Code-Augmentation/NewVerPythonDebugger(ref)/testAugmentedCode.py
@@ -7,34 +7,36 @@ def listdir_fullpath(d):
     return [f for f in os.listdir(d)]
 
 
-def augmentCode(sourcefileX, changeVariable,permuStatement, task=1 ):
-    data=pyd.readJavaFile(sourcefileX)
+def augmentCode(sourcefileX, changeVariable, permuStatement, task=1):
+    data = pyd.readJavaFile(sourcefileX)
     tree = javalang.parse.parse(data)
-    newCode,_, _ = pyd.AST(tree).getAugmentedCode(task = task, changeVariable=changeVariable, permuStatement=permuStatement)
+    newCode, _, _ = pyd.AST(tree).getAugmentedCode(
+        task=task, changeVariable=changeVariable, permuStatement=permuStatement)
     return newCode
 
 
-if __name__ == '__main__' :
-    source_dir = "NormalizedCODES" #the source codes
-    dest_dir = "AugmentedCODES" #store the augmented files
+if __name__ == '__main__':
+    source_dir = "NormalizedCODES"  # the source codes
+    dest_dir = "AugmentedCODES"  # store the augmented files
     cases = listdir_fullpath(source_dir)
     copyNum = 1
     for case in cases:
         sourcefiledir = source_dir + "\\" + case
         destfiledir = dest_dir + "\\" + case
         if not os.path.exists(destfiledir):
-            os.mkdir(destfiledir)  #create dest case dir, e.g. XX/CASE
+            os.mkdir(destfiledir)  # create dest case dir, e.g. XX/CASE
         sourcefiles = listdir_fullpath(sourcefiledir)
-        for sourcefile in sourcefiles: # .java
-            if not sourcefile.lower().endswith(".java"): continue
+        for sourcefile in sourcefiles:  # .java
+            if not sourcefile.lower().endswith(".java"):
+                continue
             for i in range(copyNum):
-                print("Augmenting case : ", case, "sourcefile: ",sourcefile, " copy num: ", i, " ......") 
-                copyfiledir = destfiledir + "\\" + str(i)  #create dest case/0 dir, e.g. XX/CASE/0
+                print("Augmenting case : ", case, "sourcefile: ",
+                      sourcefile, " copy num: ", i, " ......")
+                # create dest case/0 dir, e.g. XX/CASE/0
+                copyfiledir = destfiledir + "\\" + str(i)
                 if not os.path.exists(copyfiledir):
-                    os.mkdir(copyfiledir)  #create dest case/0 dir
+                    os.mkdir(copyfiledir)  # create dest case/0 dir
                     destfile = copyfiledir + "\\" + sourcefile
-                    newCode = augmentCode(sourcefiledir+"\\"+sourcefile, True, True, 1)
+                    newCode = augmentCode(
+                        sourcefiledir + "\\" + sourcefile, True, True, 1)
                     pyd.write_to_file(newCode, destfile)
-
-
-        
